0f07f47ff659b2488d9a2f0cac4e40f40d55358c
pocket-red-fox <pocket.red.fox@gmail.com>
2023-08-18 05:03:05 +0300

    git: remove unnecessaty platforms from git

--- lib/src/data/datasources/strapi/authorization_strapi_datasource.dart
+++ lib/src/data/datasources/strapi/authorization_strapi_datasource.dart
@@ -1,4 +1,19 @@
+import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 
 @Injectable()
-class AuthorizationStrapiDatasource {}
+class AuthorizationStrapiDatasource {
+  static const String callbackUrl = '/auth/local';
+
+  final Dio dio;
+
+  AuthorizationStrapiDatasource({
+    required this.dio,
+  });
+
+  Future<void> callback() async {
+    dio.post(
+      '',
+    );
+  }
+}
